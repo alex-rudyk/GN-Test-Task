9a8dfb929dac11720899d2fa7ffc8547ddfa1e7b
Oleksandr Rudyk <orudyk@impressit.io>
2021-11-03 13:52:51 +0200

    Add some comments

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ const { doSomeLogic } = require("../services/appGN");
 exports.appGNCtr = async (req, res) => {
 	const number = parseInt(req.params.number);
 
+	// Check our params.
 	if (!number || typeof number !== 'number') {
 		res.status(400).json({ message: 'Param must be a number' })
 		return;
--- services/appGN.js
+++ services/appGN.js
@@ -2,12 +2,15 @@
 exports.doSomeLogic = (number) => {
 	let output = '';
 
+	// If the number is a multiple of 3, add 'G' to the output string
 	if (number % 3 === 0)
 		output += 'G';
 
+	// If the number is a multiple of 5, add 'N' to the output string
 	if (number % 5 === 0)
 		output += 'N';
 
+	// If the output string is an empty number does not meet any of the conditions. Add to the output string this number
 	if (output.length < 1)
 		output = number.toString();
 
