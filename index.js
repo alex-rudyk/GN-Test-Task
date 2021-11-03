75ab257ecbdf4b025d717534dccf77d012b68533
Oleksandr Rudyk <orudyk@impressit.io>
2021-11-03 13:29:00 +0200

    Setup ExpressJS

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello GN!')
+const express = require('express');
+const config = require('./config');
+
+function createExpressApp() {
+	const app = express();
+
+	app.use(express.json());
+
+	return app;
+}
+
+const app = createExpressApp();
+
+app.listen(config.port, () => {
+	console.log(`Hello GN!\nServer listening in the port: ${config.port}`);
+});
