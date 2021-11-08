dc81462af4b6a24f2d2d2db3ba266d10357f9300
Oleksandr Rudyk <orudyk@impressit.io>
2021-11-08 11:14:05 +0200

    Clean up useless Http status

--- utils/httpException.js
+++ utils/httpException.js
@@ -9,8 +9,6 @@ exports.HttpException = class HttpException extends Error {
 
 exports.HttpStatus = {
 	OK: 200,
-	CREATED: 201,
 	BAD_REQUEST: 400,
-	UNAUTHORIZED: 401,
 	INTERNAL_SERVER_ERROR: 500
 }
